a201de42f17546ee7f6ac1391bd4f40914bf7038
Andrew Trotman <andrew@trotman.com>
2021-05-14 08:51:43 +1200

    Initial Commit

---
+++ cosc345/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  cosc345
+//
+//  Created by Andrew Trotman on 14/05/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ cosc345/cosc345App.swift
@@ -0,0 +1,17 @@
+//
+//  cosc345App.swift
+//  cosc345
+//
+//  Created by Andrew Trotman on 14/05/21.
+//
+
+import SwiftUI
+
+@main
+struct cosc345App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
